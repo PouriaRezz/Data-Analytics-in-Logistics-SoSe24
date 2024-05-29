2a6ec470f294a368cc8de53f384c6787922167c3
Franka Schlösser <saknarf@web.de>
2024-05-29 08:51:42 +0200

    Add check for empty table to import

--- SMN/_import2database.sh
+++ SMN/_import2database.sh
@@ -14,9 +14,14 @@ CREATE TABLE IF NOT EXISTS SMN (
 );
 EOF
 
+COUNT=$(sqlite3 SMN.db "SELECT COUNT(*) FROM SMN;");
 
+if [ $((COUNT)) -eq 0 ]
+then
 # Daten importieren
-sqlite3 -cmd ".mode tabs" SMN.db ".import routes.dat SMN"
+sqlite3 -cmd ".mode tabs" SMN.db ".import --skip 1 routes.dat SMN"
 sqlite3 SMN.db "UPDATE SMN SET SORTER_JOB_COMPLETE_INCON_SRC = NULL WHERE SORTER_JOB_COMPLETE_INCON_SRC = '';"
 sqlite3 SMN.db "CREATE INDEX LOT_IDX ON SMN(LOT);"
 sqlite3 SMN.db "CREATE INDEX ROUTE_IDX ON SMN(ROUTE);"
+
+fi
