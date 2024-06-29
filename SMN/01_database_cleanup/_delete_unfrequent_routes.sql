ef56e0af17a5ad3f29d4f03b3baacbcbc1110618
Franka Schlösser <saknarf@web.de>
2024-06-29 13:15:50 +0200

    Fix Makefile dependencies, add cleanup of unfrequent routes

---
+++ SMN/01_database_cleanup/_delete_unfrequent_routes.sql
@@ -0,0 +1,23 @@
+WITH selected_num_lots AS (
+    SELECT
+        ROUTE,
+        COUNT(DISTINCT LOT) as num_lots
+    FROM SMN
+    GROUP BY ROUTE
+    ORDER BY num_lots ASC
+    LIMIT CAST(0.33*(SELECT COUNT(DISTINCT SMN.ROUTE) FROM SMN) AS INTEGER)
+), lot_threshold AS (
+    SELECT
+        MAX(num_lots) threshold
+    FROM selected_num_lots
+), filtered_out_routes AS (
+    SELECT
+        ROUTE,
+        COUNT(DISTINCT LOT) as num_lots
+    FROM SMN, lot_threshold
+    GROUP BY ROUTE
+    HAVING num_lots < threshold
+)
+DELETE
+FROM SMN
+WHERE ROUTE IN (SELECT ROUTE FROM filtered_out_routes);
---
+++ SMN/01_database_cleanup/_select_limit_unfrequent_routes.sql
@@ -0,0 +1,14 @@
+-- select bottom x% routes ordered by number of lots that take them
+WITH selected_num_lots AS (
+    SELECT
+        ROUTE,
+        COUNT(DISTINCT LOT) as num_lots
+    FROM SMN
+    GROUP BY ROUTE
+    ORDER BY num_lots ASC
+    LIMIT CAST(0.33*(SELECT COUNT(DISTINCT SMN.ROUTE) FROM SMN) AS INTEGER)
+)
+SELECT
+    MAX(num_lots) threshold
+FROM selected_num_lots;
+
